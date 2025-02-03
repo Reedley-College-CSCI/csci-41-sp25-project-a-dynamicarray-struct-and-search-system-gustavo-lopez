87cae4dd8821e45cb5bae59f984458da99c21553
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-02 20:58:54 -0800

    Added class

--- PlayStation.h
+++ PlayStation.h
@@ -1,1 +1,44 @@
-#pragma once
+#ifndef PLAYSTATION_H
+#define PLAYSTATION_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class PlayStationGame {
+private:
+	string title;
+	string genre;
+	string devName;
+	string publisherName;
+	int releaseDay;
+	int releaseMonth;
+public:
+	//constructors and destructors
+	PlayStationGame();
+	PlayStationGame(string title);
+	PlayStationGame(int listArray[]);
+
+	//setters and getters
+	getTitle(const  )
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
