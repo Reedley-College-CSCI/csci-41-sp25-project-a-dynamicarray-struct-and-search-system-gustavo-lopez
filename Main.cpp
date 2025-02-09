55ad1ba4330695914ebfc59db1a07e0651a8e7fc
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-09 15:08:57 -0800

    Update Main.cpp
    Added switch statements
    Created a pointer object for the PlayStation class
    deleted object1 directly in main but it's probably better to have a function do that.

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 /*
 Gustavo Lopez Celestino
-For Project A, I am going to take the first 5 games from every letter in the catalog. So I will put in the first 5 games
-from letter A, 5 games from letter B, 5 games from letter C, and so on. So the catalog will be 130 games at first.
+For Project A, I am going to take the first 2 games from every letter in the catalog. So I will put in the first 2 games
+from letter A, 2 games from letter B, 2 games from letter C, and so on. So the catalog will be 52 games at first.
 */
 #include <iostream>
 #include "PlayStation.h"
@@ -10,14 +10,35 @@ from letter A, 5 games from letter B, 5 games from letter C, and so on. So the c
 using namespace std;
 
 int main() {
-	PlayStation object1(100);
-	
-	object1.displayMainMenu();
-	object1.readInGameInfo("PlaystationGames.txt");
+	PlayStation* object1 = new PlayStation(100);
+	int mainMenuChoice;
+	string gameToFind;
 
+	//line to read in text files
+	object1->readInGameInfo("PlaystationGames.txt");
+	object1->displayMainMenu();
 
+	do {
+		cin >> mainMenuChoice;
 
+		switch (mainMenuChoice) {
+			case 1:
+				object1->displayGames();
+				break;
+			case 2:
+				cout << "Enter game you want to find: ";
+				cin >> gameToFind;
+				//object1.findGame(gameToFind);
+				break;
+			case 3:
+				cout << "Exiting program" << endl;
+				break;
+			default:
+				cout << "Invalid choice. Please try again." << endl;
+		}
+	} while (mainMenuChoice != 3);
 
+	delete object1;
 	return 0;
 }
 
