7f3fec00680d4f394dee35827d66866770872aa0
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-08 22:33:14 -0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+/*
+Gustavo Lopez Celestino
+
+For Project A, I am going to take the first 2 games from every letter in the catalog. So I will put in the first 2 games
+from letter A, 2 games from letter B, 2 games from letter C, and so on. So the catalog will be 52 games at first.
+*/
+
+#include <iostream>
+#include "PlayStation.h"
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+	PlayStation object1;
+
+	cout << object1.getTitle();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
