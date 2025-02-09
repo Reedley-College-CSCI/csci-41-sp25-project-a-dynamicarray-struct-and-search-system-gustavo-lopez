3ad925ac92a55cfb4fedd0321010612fc9495ca1
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-09 01:41:13 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,8 @@
 /*
 Gustavo Lopez Celestino
-
-For Project A, I am going to take the first 2 games from every letter in the catalog. So I will put in the first 2 games
-from letter A, 2 games from letter B, 2 games from letter C, and so on. So the catalog will be 52 games at first.
+For Project A, I am going to take the first 5 games from every letter in the catalog. So I will put in the first 5 games
+from letter A, 5 games from letter B, 5 games from letter C, and so on. So the catalog will be 130 games at first.
 */
-
 #include <iostream>
 #include "PlayStation.h"
 #include <string>
@@ -12,19 +10,10 @@ from letter A, 2 games from letter B, 2 games from letter C, and so on. So the c
 using namespace std;
 
 int main() {
-	PlayStation object1;
-
-	cout << object1.getTitle();
-
-
-
-
-
-
-
-
-
-
+	PlayStation object1(100);
+	
+	object1.displayMainMenu();
+	object1.readInGameInfo("PlaystationGames.txt");
 
 
 
