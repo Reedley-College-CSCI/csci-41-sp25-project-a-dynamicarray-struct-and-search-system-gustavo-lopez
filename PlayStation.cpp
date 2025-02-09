c36dcdfcd32e6d9ea97a5544310eb5125ac7ce3d
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-09 01:48:04 -0800

    Update PlayStation.cpp
    Added readInGameInfo function and a way to display the menu to the user. Tweaked some other things too

--- PlayStation.cpp
+++ PlayStation.cpp
@@ -1,22 +1,50 @@
 #include "PlayStation.h"
+#include <fstream>
+#include <string>
 
 //default constructor
 PlayStation::PlayStation() {
-	title = "No title";
-	genre = "No genre";
-	devName = "No developer";
-	publisherName = "No publisher";
-	releaseMonth = 01;
-	releaseDay = 01;
-	releaseYear = 2013;
+	cout << "Default constructor called" << endl;
+}
+PlayStation::PlayStation(int arraySize) {
+	currListOfGames = new GameAttributes[arraySize];
+} 
+
+//destructors
+PlayStation::~PlayStation() {
+	cout << "Destructor called" << endl;
+
+	delete[] currListOfGames;
 }
 
-//getTitle() function that returns title of a game.
-string PlayStation::getTitle() {
-	return title;
+void PlayStation::displayMainMenu() {
+	cout << "Welcome user! This program holds PlayStation 4 games in a catalog. Currently, we have" <<
+		" 52 titles with their respective information. Feel free to browse at your leisure." << endl;
+	cout << endl << "1. Display All Titles" << endl;
+	cout << "2. Search For A Game" << endl;
+	cout << "3. Exit the program" << endl;
+	cout << "Choose an option: ";
+
+
 }
 
-PlayStation::~PlayStation() {}
+void PlayStation::readInGameInfo(const string& extFileName) {
+	ifstream inFile(extFileName);
+	string line;
 
 
+	if (inFile.is_open()) {
+		while (getline(inFile, line)) {
+			cin >> line;
+		}
+		
+		inFile.close();
 
+		cout << "Game info has been read in and the file has been closed." << endl;
+	}
+	else {
+		cout << "Unable to open file" << endl;
+	}
+
+	
+}
