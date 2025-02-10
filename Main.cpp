9ffc14945d027c8e73abd39e911c08bfdf7dea78
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-10 15:50:09 -0800

    Update Main.cpp
    Got rid of the switch statement in main and turned it into a function for better modularity
    Initialized mainMenuChoice to zero rather than having user enter it in main

--- Main.cpp
+++ Main.cpp
@@ -10,48 +10,14 @@ from letter A, 2 games from letter B, 2 games from letter C, and so on. So the c
 using namespace std;
 
 int main() {
-	PlayStation* object1 = new PlayStation(100);
-	int mainMenuChoice;
+	PlayStation* object1 = new PlayStation(500);
+	int mainMenuChoice = 0;
 	string gameToFind;
 	GameAttributes* foundGame = nullptr;
 
-	//line to read in text files
 	object1->readInGameInfo("PlaystationGames.txt");
 	object1->displayMainMenu();
-
-	do {
-		cin >> mainMenuChoice;
-		while (cin.get() != '\n');
-
-		switch (mainMenuChoice) {
-			case 1:
-				object1->displayGames();
-				break;
-			case 2: 
-				cout << "Enter name of the game you want to find: ";
-				getline(cin, gameToFind);
-				foundGame = object1->findGame(gameToFind);
-
-				// Check if the game was found
-				if (foundGame) {
-					cout << "Game found: " << foundGame->gameTitle << endl;
-					cout << "Genre: " << foundGame->genre << endl;
-					cout << "Developer: " << foundGame->devName << endl;
-					cout << "Publisher: " << foundGame->publisherName << endl;
-					cout << "Release Date: " << foundGame->releaseDate << endl;
-				}
-				else {
-					cout << "Game not found." << endl;
-				}
-				break;
-			case 3:
-				cout << "Exiting program" << endl;
-				break;
-			default:
-				cout << "Invalid choice. Please try again." << endl;
-		}
-		object1->displayMainMenu();
-	} while (mainMenuChoice != 3);
+	object1->displaySubMenu(object1, mainMenuChoice);
 
 	delete object1;
 	return 0;
