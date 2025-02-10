af28d11be0a61835aa1acfe1039d8e278262aaec
GustavoLopez999 <gustavolopez1574@gmail.com>
2025-02-10 15:52:03 -0800

    Update PlayStation.cpp
    Added a submenu to get rid of the bulky switch statement in main.cpp
    Added comments for better readability
    Rewrote menu code so the initial main menu doesn't pop up again after the user makes a choice

--- PlayStation.cpp
+++ PlayStation.cpp
@@ -5,35 +5,38 @@
 //default constructor
 PlayStation::PlayStation() {
 	cout << "Default constructor called" << endl;
-	//concrete array size, need to make it dynamic to be able to change the size of the array.
 	arraySize = 100;
 	currentGamesInArray = 0;
 	currListOfGames = new GameAttributes[arraySize];
 }
 
-//constructor to edit array size
+//constructor to edit arraySize struct attribute
 PlayStation::PlayStation(int size) {
 	arraySize = size;
 	currListOfGames = new GameAttributes[arraySize];
 } 
 
-//destructors
+//destructor for a PlayStation object that's created.
 PlayStation::~PlayStation() {
-	cout << "Destructor called" << endl;
 
+	cout << "Destructor called" << endl;
 	delete[] currListOfGames;
 }
 
+//as the name states, this function displays the main menu
 void PlayStation::displayMainMenu() {
 	cout << "Welcome user! This program holds PlayStation 4 games in a catalog. Currently, we have" <<
 		" 52 titles with their respective \ninformation. Feel free to browse at your leisure." << endl;
-	cout << endl << "1. Display All Titles" << endl;
-	cout << "2. Search For A Game" << endl;
-	cout << "3. Exit the program" << endl;
+	cout << endl << "1. Display all titles" << endl;
+	cout << "2. Search for a game" << endl;
+	cout << "Enter -1 to close the program." << endl;
+
 	cout << "Choose an option: ";
 }
 
 //this function will read in game information from my ext file and write them into my currGameList array
+//line by line. I used commas as the seperator for titles, genres, publishers, etc. Easier to iterate
+//through and sort accordingly.
 void PlayStation::readInGameInfo(const string& extFileName) {
 	ifstream inFile(extFileName);
 	string line;
@@ -106,9 +109,57 @@ GameAttributes* PlayStation::findGame(const string& userGameTitle) const {
 	return nullptr;
 }
 
+void PlayStation::displaySubMenu(PlayStation* psObject, int& menuChoice) {
+	//move do while loop into it's own separate function for better modularity
+	string gameToFind;
+	GameAttributes* foundGame = nullptr;
 
+	do {
+		cin >> menuChoice;
+		while (cin.get() != '\n');
 
+		switch (menuChoice) {
+		case 1:
+			psObject->displayGames();
+			break;
+		case 2:
+			cout << "Enter name of the game you want to find: ";
+			getline(cin, gameToFind);
+			foundGame = psObject->findGame(gameToFind);
+
+			// Check if the game was found. Should make this a function for better modularity
+			if (foundGame) {
+				cout << endl << "Game found: " << foundGame->gameTitle << endl;
+				cout << "Genre: " << foundGame->genre << endl;
+				cout << "Developer: " << foundGame->devName << endl;
+				cout << "Publisher: " << foundGame->publisherName << endl;
+				cout << "Release Date: " << foundGame->releaseDate << endl << endl;
+			}
+			else {
+				cout << "Game not found." << endl;
+			}
+			break;
+		case -1:
+			cout << endl << "Exiting program" << endl;
+			break;
+		default:
+			cout << "Invalid choice. Please try again." << endl;
+		}
+		if (menuChoice != -1) {
+			cout << "You're back here? Go back and check out one of the other options or hit 3 to leave.";
+			cout << endl << endl << "1. Display all titles" << endl;
+			cout << "2. Search for a game" << endl;
+			cout << "Enter -1 to exit the program (you don't have to though)" << endl;
+			cout << "Choose an option: ";
+		}
+	} while (menuChoice != -1);
+
+}
+
+//FINISH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //function to sort games A-Z. Use binary search since the array is fairly large.
 void PlayStation::displayGamesAlphabetically() const {
 	cout << "Called displayGamesAlphabetically function" << endl;
 }
+
+
